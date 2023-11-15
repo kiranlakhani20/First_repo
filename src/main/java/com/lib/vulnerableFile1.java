d2caa86967d990afa96a99c180b4c7059a65b154
kiranlakhani20 <55124757+kiranlakhani20@users.noreply.github.com>
2023-11-15 18:29:09 +0530

    Update vulnerableFile1.java

--- src/main/java/com/lib/vulnerableFile1.java
+++ src/main/java/com/lib/vulnerableFile1.java
@@ -1,1 +1,24 @@
-
+package com.training.example.JacocoExample;
+public class Palindrome2 {
+	
+	public boolean isPalindrome(String inputString) {
+	    if (inputString.length() == 0) {
+	        return true;
+	    } else {
+	    	String rev = "";
+			String rev2 = "";
+	    	int length = inputString.length();
+	    	 
+	        for ( int i = length - 1; i >= 0; i-- )
+	           rev = rev + inputString.charAt(i);
+	   		//check rev string
+	        if (inputString.equals(rev))
+	           return true;
+			else if (inputString.equalsIgnoreCase(rev))
+	           return true;
+	        else
+	           return false;
+			
+	    }
+	}
+}
